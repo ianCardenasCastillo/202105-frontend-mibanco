e8bd787824f9cfd62a4fb2c04d454a802fbe4ae4
Your-Full-Name <render3dmente@gmail.com>
2021-04-29 21:23:47 -0400

    Se crea el servicio para obtener los bancos existentes

---
+++ src/app/services/bank-list.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BankListService {
+  private baseLink= 'https://bast.dev/'
+
+  constructor(public http: HttpClient) { }
+
+  getBanks(){
+    return this.http.get(this.baseLink+'api/banks.php');
+  }
+
+}
