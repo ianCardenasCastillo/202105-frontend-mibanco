315dae1bde57ba374564f3a822753c3de1a82211
Your-Full-Name <render3dmente@gmail.com>
2021-04-29 21:25:29 -0400

    Se agrega la ruta a distinatarios y se define la visual principal en app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDestinatarioComponent } from './add-destinatario/add-destinatario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '',   redirectTo: '/destinatarios', pathMatch: 'full' },
+  {
+    path:'destinatarios',
+    component: AddDestinatarioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
